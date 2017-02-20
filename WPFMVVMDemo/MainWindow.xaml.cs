7ec63eeeecd69a447ca1390bcdbf6a3e96cb3542
K8721 <K8721@ICT-421-WS-15>
2017-02-20 12:52:28 +0200

    MVVM

---
+++ WPFMVVMDemo/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFMVVMDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WPFMVVMDemo.ViewModel.StudentViewModel svmo = new ViewModel.StudentViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            svmo.LoadStudents();
+        }
+
+        private void StudentViewControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            StudentViewControl.DataContext = svmo;
+        }
+
+        private void dgStudents_Loaded(object sender, RoutedEventArgs e)
+        {
+            dgStudents.DataContext = svmo.Students;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            //luodaan uusi Student-olio observablecolllectioniin
+            WPFMVVMDemo.Model.Student uusi = new Model.Student();
+            uusi.FirstName = txtFirstName.Text;
+            uusi.LastName = txtLastName.Text;
+            svmo.Students.Add(uusi);
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+        }
+    }
+}
---
+++ WPFMVVMDemo/ViewModel/StudentViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFMVVMDemo.Model;
+
+namespace WPFMVVMDemo.ViewModel
+{
+    public class StudentViewModel
+    {
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+            //lisätään esimerkin vuoksi muutama opiskelija, oikeassa sovelluksessa tulisivat vaikka tietokannasta
+            students.Add(new Student { FirstName = "Kalle", LastName = "Jalkanen", AsioId="1234543" });
+            students.Add(new Student { FirstName = "Teppo", LastName = "Testaaja", AsioId = "rewqwerewq" });
+            students.Add(new Student { FirstName = "Tomi", LastName = "Töttersätänst", AsioId = "fdsasdfdwsa" });
+            students.Add(new Student { FirstName = "Anni", LastName = "Anjolkainen", AsioId = "bvcdzxcv" });
+            Students = students;
+        }
+    }
+}
